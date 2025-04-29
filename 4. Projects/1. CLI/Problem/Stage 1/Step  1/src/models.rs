dc3ffa27971b88dd8f24e61b45b8e533fa16f9b0
Nicholas Werhrhan <nwehrhan@noip.com>
2025-04-29 12:29:51 +0200

    Finish step 1

--- 4. Projects/1. CLI/Problem/Stage 1/Step  1/src/models.rs
+++ 4. Projects/1. CLI/Problem/Stage 1/Step  1/src/models.rs
@@ -1,48 +1,51 @@
 use std::collections::HashMap;
 
 pub enum Status {
-    Open,
     InProgress,
-    Resolved,
-    Closed
+    Closed,
+    Open
 }
 
 pub struct Epic {
-    pub name: String,
-    pub description: String,
-    pub status: Status,
-    pub stories: Vec<u32>,
+    name: String,
+    description: String,
+    status: Status,
+    stories: Vec<i32>
 }
 
 impl Epic {
     pub fn new(name: String, description: String) -> Self {
-        Self {
-            name,
-            description,
+        // by default the status should be set to open and the stories should be an empty vector
+        Epic {
+            name: name,
+            description: description,
             status: Status::Open,
-            stories: vec![]
+            stories: Vec::new()
         }
     }
 }
 
 pub struct Story {
-    pub name: String,
-    pub description: String,
-    pub status: Status,
+    name: String,
+    description: String,
+    status: Status
 }
 
 impl Story {
     pub fn new(name: String, description: String) -> Self {
-        Self {
-            name,
-            description,
+        // by default the status should be set to open
+        Story {
+            name: name,
+            description: description,
             status: Status::Open,
         }
     }
 }
 
 pub struct DBState {
-    pub last_item_id: u32,
-    pub epics: HashMap<u32, Epic>,
-    pub stories: HashMap<u32, Story>
+    // This struct represents the entire db state which includes the last_item_id, epics, and stories
+    // TODO: add fields (make sure the fields are public)
+    last_item_id: i32,
+    epics: Vec<Epic>,
+    stories: Vec<Story>
 }
